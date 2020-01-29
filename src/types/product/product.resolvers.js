29ae02025e8f4eab2392ffb8d0038548ce495597
rachit2501 <rac.sri25@gmail.com>
2020-01-29 22:14:59 +0530

    basic structure

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -1,7 +1,7 @@
 import { Product } from './product.model'
-import { User } from '../user/user.model'
+import { User, roles } from '../user/user.model'
 import { AuthenticationError } from 'apollo-server'
-import { roles } from '../../utils/auth'
+import mongoose from 'mongoose'
 
 const productsTypeMatcher = {
   GAMING_PC: 'GamingPc',
@@ -9,69 +9,10 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
-/** product */
-const product = (_, args, ctx) => {
-  if (!ctx.user) {
-    throw new AuthenticationError()
-  }
-  return Product.findById(args.id)
-    .lean()
-    .exec()
-}
-
-const newProduct = (_, args, ctx) => {
-  if (!ctx.user || ctx.user.role !== roles.admin) {
-    throw new AuthenticationError()
-  }
-  return Product.create({ ...args.input, createdBy: ctx.user._id })
-}
-
-const products = (_, args, ctx) => {
-  if (!ctx.user) {
-    throw new AuthenticationError()
-  }
-  return Product.find({})
-    .lean()
-    .exec()
-}
-
-const updateProduct = (_, args, ctx) => {
-  if (!ctx.user || ctx.user.role !== roles.admin) {
-    throw new AuthenticationError()
-  }
-  const update = args.input
-  return Product.findByIdAndUpdate(args.id, update, { new: true })
-    .lean()
-    .exec()
-}
-
-const removeProduct = (_, args, ctx) => {
-  if (!ctx.user || ctx.user.role !== roles.admin) {
-    throw new AuthenticationError()
-  }
-  return Product.findByIdAndRemove(args.id)
-    .lean()
-    .exec()
-}
-
 export default {
-  Query: {
-    products,
-    product
-  },
-  Mutation: {
-    newProduct,
-    updateProduct,
-    removeProduct
-  },
+  Query: {},
+  Mutation: {},
   Product: {
-    __resolveType(product) {
-      return productsTypeMatcher[product.type]
-    },
-    createdBy(product) {
-      return User.findById(product.createdBy)
-        .lean()
-        .exec()
-    }
+    __resolveType(product) {}
   }
 }
