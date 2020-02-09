e880e6a3858802c28a980f32ef6b0415f7158e16
rachit2501 <rac.sri25@gmail.com>
2020-02-09 17:35:03 +0530

    product resolvers

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -1,7 +1,6 @@
 import { Product } from './product.model'
 import { User, roles } from '../user/user.model'
 import { AuthenticationError } from 'apollo-server'
-import mongoose from 'mongoose'
 
 const productsTypeMatcher = {
   GAMING_PC: 'GamingPc',
@@ -9,10 +8,68 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const product = (_, args, ctx) => {
+  if (!ctx.user) {
+    throw new AuthenticationError()
+  }
+  return Product.findById(args.id)
+    .lean()
+    .exec()
+}
+
+const newProduct = (_, args, ctx) => {
+  if (!ctx.user || ctx.user.role !== roles.admin) {
+    throw new AuthenticationError()
+  }
+  return Product.create({ ...args.input, createdBy: ctx.user._id })
+}
+
+const products = (_, args, ctx) => {
+  if (!ctx.user) {
+    throw new AuthenticationError()
+  }
+  return Product.find({})
+    .lean()
+    .exec()
+}
+
+const updateProduct = (_, args, ctx) => {
+  if (!ctx.user || ctx.user.role !== role.admin) {
+    throw new AuthenticationError()
+  }
+
+  const update = args.input
+  return Product.findByIdAndUpdate(args.id, update, { new: true })
+    .lean()
+    .exec()
+}
+
+const removeProduct = (_, args, ctx) => {
+  if (!ctx.user || ctx.user.role !== roles.admin) {
+    throw AuthenticationError()
+  }
+  return Product.findByIdAndRemove(args.id)
+    .lean()
+    .exec()
+}
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    products,
+    product
+  },
+  Mutations: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {
+      return productsTypeMatcher[product.type]
+    },
+    createdBy(product) {
+      return User.findById(product.createdBy)
+        .lean()
+        .exec()
+    }
   }
 }
